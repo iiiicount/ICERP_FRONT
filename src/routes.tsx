7690a8a5629504389dcce4115fa42f20611cfebd
정상현 <jkn17083@gmail.com>
2023-02-09 19:08:29 +0900

    add : freeboardComponent, freeboardpage
    (cherry picked from commit 8cecd2683f6fe09670d65352746e22915bf5071c)

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,28 +3,33 @@ import LoginPage from "./pages/LoginPage";
 import NoticePage from "./pages/NoticePage";
 import SalePage from "./pages/SalePage";
 import PurchasePage from "./pages/PurchasePage";
+import FreeBoardPage from "./pages/FreeBoardPage";
 
 const routes = [
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-        path: "/login",
-        element: <LoginPage />,
-    },
-    {
-        path: "/notice",
-        element: <NoticePage />,
-    },
-    {
-        path: "/sale",
-        element: <SalePage />,
-    },
-    {
-        path: "/purchase",
-        element: <PurchasePage />,
-    },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/notice",
+    element: <NoticePage />,
+  },
+  {
+    path: "/sale",
+    element: <SalePage />,
+  },
+  {
+    path: "/purchase",
+    element: <PurchasePage />,
+  },
+  {
+    path: "/freeboard",
+    element: <FreeBoardPage />,
+  },
 ];
 
 export default routes;
