f2bf1a00e9f84bf54bdecca77a7102721d647976
minju <rlaalswn3282@kookmin.ac.kr>
2023-03-23 20:06:17 +0900

    Merge branch 'main' of https://github.com/iiiicount/ICERP_FRONT into minju

--- src/pages/FreeBoardPage.tsx
+++ src/pages/FreeBoardPage.tsx
@@ -1,44 +1,78 @@
 import ContentBox from "../components/ContentBox";
 import ContentTop from "../components/ContentTop";
+import styles from "../assets/scss/FreeBoardPage.module.scss";
+import SideBar from "../components/SideBar";
+import TabContent from "../components/TabContent";
+import BasicButton from "../components/BasicButton";
+import { HiOutlineChatBubbleOvalLeftEllipsis } from "react-icons/hi2";
+import { contentBoxListDatas } from "../assets/data/MockDatas";
+import { Link } from "react-router-dom";
 
-const contentBoxList = [
+//목데이터 가져오기
+const contentBoxList = contentBoxListDatas;
+const importantContents = contentBoxList.filter((contentBox) => {
+  return contentBox.type == "notification";
+});
+
+const category = [
   {
-    writer: "하하",
-    title: "안녕하세요",
-    contents: "반갑습니다 저는 하하 입니다",
+    name: "전체게시판",
+    content: contentBoxList.map((contentbox, index) => {
+      return (
+        <ContentBox
+          key={index}
+          id={contentbox.id}
+          writer={contentbox.writer}
+          title={contentbox.title}
+          contents={contentbox.contents}
+          commentCnt={contentbox.commentCnt}
+        />
+      );
+    }),
   },
   {
-    writer: "재석",
-    title: "이게 맞나요?",
-    contents: "반갑습니다 저는 재석 입니다",
-  },
-  {
-    writer: "미주",
-    title: "너무 피곤해요",
-    contents: "반갑습니다 저는 미주 입니다",
-  },
-  {
-    writer: "준서",
-    title: "맞겠지?",
-    contents: "반갑습니다 저는 준서 입니다",
+    name: "공지 사항",
+    content: importantContents.map((contentbox, index) => {
+      return (
+        <ContentBox
+          key={index}
+          id={contentbox.id}
+          writer={contentbox.writer}
+          title={contentbox.title}
+          contents={contentbox.contents}
+          commentCnt={contentbox.commentCnt}
+        />
+      );
+    }),
   },
 ];
 
 const FreeBoardPage = () => {
   return (
     <div className="go_content">
-      <ContentTop titleName={"자유 게시판"} path={"/"} />
-      <div>
-        {contentBoxList.map((contentbox, index) => {
-          return (
-            <ContentBox
-              key={index}
-              writer={contentbox.writer}
-              title={contentbox.title}
-              contents={contentbox.contents}
-            />
-          );
-        })}
+      <ContentTop titleName={"커뮤니티 > 게시판"} path={"/freeboard"} />
+      <div className={styles.freeboardpage_container}>
+        <div className={styles.contents_container}>
+          <TabContent category={category} />
+        </div>
+        <div className={styles.sidebar_container}>
+          <SideBar>
+            <BasicButton text="글쓰기" path="/freeboard/editfreeboard" />
+            <div className={styles.sidebar_header}>공지 사항</div>
+            {importantContents.map((contentbox, index) => {
+              return (
+                <Link to={`/detailfreeboardpage/${contentbox.id}`}>
+                  <div key={index} className={styles.sidebar_contents}>
+                    {contentbox.title}&nbsp;
+                    <HiOutlineChatBubbleOvalLeftEllipsis />
+                    &nbsp;
+                    {contentbox.commentCnt}
+                  </div>
+                </Link>
+              );
+            })}
+          </SideBar>
+        </div>
       </div>
     </div>
   );
