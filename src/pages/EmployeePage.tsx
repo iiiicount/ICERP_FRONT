13ece41500b890e9156c09ea6c12201fc80e1c8b
minju <rlaalswn3282@kookmin.ac.kr>
2023-03-23 20:06:17 +0900

    Merge branch 'main' of https://github.com/iiiicount/ICERP_FRONT into minju

---
+++ src/pages/EmployeePage.tsx
@@ -0,0 +1,13 @@
+import ContentTop from "../components/ContentTop";
+
+const EmployeePage = () => {
+    return (
+        <>
+            <div className="go_content">
+                <ContentTop titleName={"사원 관리"} path={"/"} />
+            </div>
+        </>
+    );
+};
+
+export default EmployeePage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,33 +3,45 @@ import LoginPage from "./pages/LoginPage";
 import NoticePage from "./pages/NoticePage";
 import SalePage from "./pages/SalePage";
 import PurchasePage from "./pages/PurchasePage";
+import EditFreeBoardPage from "./pages/EditFreeBoardPage";
+import DetailFreeBoardPage from "./pages/DetailFreeBoardPage";
 import FreeBoardPage from "./pages/FreeBoardPage";
+import CompanyPage from "./pages/CompanyPage";
+import EmployeePage from "./pages/EmployeePage";
 
 const routes = [
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/notice",
-    element: <NoticePage />,
-  },
-  {
-    path: "/sale",
-    element: <SalePage />,
-  },
-  {
-    path: "/purchase",
-    element: <PurchasePage />,
-  },
-  {
-    path: "/freeboard",
-    element: <FreeBoardPage />,
-  },
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/notice",
+        element: <NoticePage />,
+    },
+    {
+        path: "/sale",
+        element: <SalePage />,
+    },
+    {
+        path: "/purchase",
+        element: <PurchasePage />,
+    },
+    {
+        path: "/freeboard",
+        element: <FreeBoardPage />,
+    },
+    {
+        path: "/freeboard/editfreeboard",
+        element: <EditFreeBoardPage />,
+    },
+    {
+        path: "/detailfreeboardpage/:freeboardpageid",
+        element: <DetailFreeBoardPage />,
+    },
 ];
 
 export default routes;
