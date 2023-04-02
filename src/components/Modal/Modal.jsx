8858c1b7025e9331b4b516c15f0683cab073e389
park songhee <songhee6392@gmail.com>
2023-04-03 02:23:33 +0900

    add : 테이블 클릭 시 발생하는 수정 모달 생성

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import styles from "../../assets/scss/Modal.module.scss";
 
-const Modal = ({ onClose, children }) => {
+const Modal = ({ title, onClose, onSaveData, children }) => {
     return (
         <div className={styles.modal}>
-            <div className={styles.modal_content}>{children}</div>
-            <button className="close-button" onClick={onClose}>
-                X
-            </button>
+            <div className={styles.modal_wrapper}>
+                <header>
+                    <h1>{title}</h1>
+                    <button className="btn_layer_x" onClick={onClose}>
+                        X
+                    </button>
+                </header>
+                <div className={styles.modal_content}>{children}</div>
+                <footer>
+                    <button className={styles.btn_major_s} onClick={onSaveData}>
+                        확인
+                    </button>
+                    <button className={styles.btn_minor_s} onClick={onClose}>
+                        취소
+                    </button>
+                </footer>
+            </div>
         </div>
     );
 };
