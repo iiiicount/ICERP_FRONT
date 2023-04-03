3c35328f651fe45eaba14e20ba5a34cfd48f6f3f
park songhee <songhee6392@gmail.com>
2023-04-04 00:24:58 +0900

    add : 페이지네이션 기본 형태 추가

--- src/components/Pagenation/Pagenation.jsx
+++ src/components/Pagenation/Pagenation.jsx
@@ -1,5 +1,39 @@
+import styles from "../../assets/scss/Pagenation.module.scss";
+
 const Pagenation = ({ currentPage, numberOfPages, onClick }) => {
-    return <div>페이지 네이션 컴포넌트</div>;
+    const numberOfPageForSet = numberOfPages;
+    const startPage = 1; //페이지네이션 맨 앞에 있는 숫자
+    return (
+        <nav className={styles.pagenation}>
+            <ul className={styles.pagenation_wrapper}>
+                <li>
+                    <div className={styles.paginate_button}>이전</div>
+                </li>
+                {Array(numberOfPageForSet)
+                    .fill(startPage)
+                    .map((value, index) => value + index)
+                    .map((pageNumber) => {
+                        return (
+                            <li
+                                key={pageNumber}
+                                className={`${
+                                    currentPage === pageNumber
+                                        ? styles.paginate_active
+                                        : ""
+                                }`}
+                            >
+                                <div onClick={() => onClick(pageNumber)}>
+                                    {pageNumber}
+                                </div>
+                            </li>
+                        );
+                    })}
+                <li>
+                    <div className={styles.paginate_button}>다음</div>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Pagenation;
