c0a91c6ee7ad709a5940af0261a6d12af58ed933
minju <rlaalswn3282@kookmin.ac.kr>
2023-05-25 18:31:41 +0900

    fix : login scss file

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,32 +1,24 @@
-import "../assets/scss/Login.scss";
+import styles from "../assets/scss/Login.module.scss";
 import logo from "../assets/images/icount_logo_color.png";
 
 const LoginPage = () => {
-    return (
-        <div className="login">
-            <div className="login_box">
-                <img src={logo} className="app_logo" alt="logo" />
-                <div className="login_form">
-                    <input
-                        className="input"
-                        type="text"
-                        placeholder="회사코드"
-                    />
-                    <input className="input" type="text" placeholder="아이디" />
-                    <input
-                        className="input"
-                        type="password"
-                        placeholder="비밀번호"
-                    />
-                </div>
-                <div className="btn_login">
-                    <a id="login_submit" href="#">
-                        로그인
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className={styles.login}>
+      <div className={styles.login_box}>
+        <img src={logo} className={styles.app_logo} alt="logo" />
+        <div className={styles.login_form}>
+          <input className={styles.input} type="text" placeholder="회사코드" />
+          <input className={styles.input} type="text" placeholder="아이디" />
+          <input className={styles.input} type="password" placeholder="비밀번호" />
         </div>
-    );
+        <div className={styles.btn_login}>
+          <a id="login_submit" href="#">
+            로그인
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
